0365abb46bd8899797fe118f5bdef77e8a2f4805
Longchuck <longtran.cmg@gmail.com>
2023-06-23 21:30:45 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import ConfirmPassword from "./components/auth/ConfirmPassword";
-import EmailVerification from "./components/auth/EmailVerification";
-import ForgetPassword from "./components/auth/ForgetPassword";
+import { Routes, Route } from "react-router-dom";
 import Signin from "./components/auth/Signin";
 import Signup from "./components/auth/Signup";
+import EmailVerification from "./components/auth/EmailVerification";
 import Home from "./components/Home";
-import NotFound from "./components/NotFound";
 import Navbar from "./components/user/Navbar";
+import ForgetPassword from "./components/auth/ForgetPassword";
+import ConfirmPassword from "./components/auth/ConfirmPassword";
+import NotFound from "./components/NotFound";
+import { useAuth } from "./hooks";
+import AdminNavigator from "./navigator/AdminNavigator";
+import SingleMovie from "./components/user/SingleMovie";
+import MovieReviews from "./components/user/MovieReviews";
+import SearchMovies from "./components/user/SearchMovies";
 
 export default function App() {
+  const { authInfo } = useAuth();
+  const isAdmin = authInfo.profile?.role === "admin";
+
+  if (isAdmin) return <AdminNavigator />;
+
   return (
     <>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth/signin" element={<Signin />} />
@@ -21,6 +30,9 @@ export default function App() {
         <Route path="/auth/verification" element={<EmailVerification />} />
         <Route path="/auth/forget-password" element={<ForgetPassword />} />
         <Route path="/auth/reset-password" element={<ConfirmPassword />} />
+        <Route path="/movie/:movieId" element={<SingleMovie />} />
+        <Route path="/movie/reviews/:movieId" element={<MovieReviews />} />
+        <Route path="/movie/search" element={<SearchMovies />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
--- src/context/index.js
+++ src/context/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import AuthProvider from "./AuthProvider";
+import MoviesProvider from "./MoviesProvider";
 import NotificationProvider from "./NotificationProvider";
+import SearchProvider from "./SearchProvider";
 import ThemeProvider from "./ThemeProvider";
 
 export default function ContextProviders({ children }) {
   return (
     <NotificationProvider>
-      <AuthProvider>
-        <ThemeProvider>{children}</ThemeProvider>
-      </AuthProvider>
+      <SearchProvider>
+        <MoviesProvider>
+          <AuthProvider>
+            <ThemeProvider>{children}</ThemeProvider>
+          </AuthProvider>
+        </MoviesProvider>
+      </SearchProvider>
     </NotificationProvider>
   );
 }
